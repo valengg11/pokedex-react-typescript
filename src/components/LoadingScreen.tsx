e662dc4af73ec35c5eb3b021f24c5ee5faf5b208
valentina <valentinagomezgiraldo11@gmail.com>
2023-02-17 11:56:35 -0500

    Add loading screen component

---
+++ src/components/LoadingScreen.tsx
@@ -0,0 +1,14 @@
+import Pokedex from "../assets/pokedex.png"
+
+import styles from "./loadingScreen.module.css"
+
+const LoadingScreen = () => {
+    return (
+        <div className={styles.loadingScreen}>
+            <img src={Pokedex} alt="Pokedex" className={styles.loadingScreenIcon}/>
+        </div>
+    )
+
+};
+
+export default LoadingScreen;
