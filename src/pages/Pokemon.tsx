37badcbb97823ca038af8198d5d04211042de81b
valentina <valentinagomezgiraldo11@gmail.com>
2023-02-17 12:49:30 -0500

    Add pokemon details fetch and style

--- src/pages/Pokemon.tsx
+++ src/pages/Pokemon.tsx
@@ -1,11 +1,55 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+
+import PokeballImg from "../assets/pokeball.png"
+import Footer from '../components/Footer';
+
+import styles from "./pokemon.module.css"
+import { useEffect, useState } from 'react';
+import { PokemonDetails } from '../types/types';
+import {fetchPokemon} from '../api/fetchPokemon';
+import LoadingScreen from '../components/LoadingScreen';
+import {waitFor} from '../utils/utils';
 
 const Pokemon = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [pokemon, setPokemon] = useState<PokemonDetails>();
     const { name } = useParams();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        async function getPokemon() {
+            setIsLoading(true);
+            await waitFor(500);
+            const fetchedPokemon = await fetchPokemon(name as string);
+            setPokemon(fetchedPokemon);
+            setIsLoading(false);
+        }
+        getPokemon();
+    }, [name]);
+
+    if (isLoading || !pokemon){
+        return <LoadingScreen />
+    };
+
     return (
-        <div>
-            { name }
-        </div>
+        <>
+            <button className={styles.pokeballButton} onClick={() => navigate(-1)}>
+                <img className={styles.pokeballImg} src={PokeballImg} alt="pokeball"/>
+            </button>
+            <div className={styles.pokemon}>
+                <main className={styles.pokemonInfo}>
+                    <div className={styles.pokemonTitle}>{pokemon?.name.toUpperCase()}</div>
+                    <div>Nr {pokemon?.id}</div>
+                    <div>
+                        <img className={styles.pokemonInfoImg} src={pokemon?.imgSrc} alt={pokemon?.name}/>
+                    </div>
+                    <div>HP: {pokemon?.hp}</div>
+                    <div>Attack: {pokemon?.attack}</div>
+                    <div>Defense: {pokemon?.defense}</div>
+                </main>
+            </div>
+            <Footer/>
+        </>
     )
 };
 
